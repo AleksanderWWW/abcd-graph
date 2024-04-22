a71f7e7048bebf35a63de3f94a01e6b81bfe8b57
AleksanderWWW <alwojnarowicz@gmail.com>
2024-04-22 19:05:02 +0200

    add logging

---
+++ src/abcd_graph/logger.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2024 Jordan Barrett
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+__all__ = [
+    "ABCDLogger",
+    "LoggerType",
+    "construct_logger",
+]
+
+import logging
+import os
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from typing import Union
+
+from typing_extensions import TypeAlias
+
+
+class ABCDLogger(ABC):
+    @abstractmethod
+    def info(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def debug(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def warning(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def error(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def critical(self, message: str) -> None:
+        pass
+
+
+class NoOpLogger(ABCDLogger):
+    def info(self, message: str) -> None:
+        pass
+
+    def debug(self, message: str) -> None:
+        pass
+
+    def warning(self, message: str) -> None:
+        pass
+
+    def error(self, message: str) -> None:
+        pass
+
+    def critical(self, message: str) -> None:
+        pass
+
+
+class StdOutLogger(ABCDLogger):
+    NAME = "abcd-graph"
+
+    def __init__(self) -> None:
+        logging_level = int(os.getenv("ABCD_LOGGING_LEVEL", logging.INFO))
+        logging.basicConfig(
+            level=logging_level,
+            format="[%(name)s] - %(asctime)s - %(levelname)s - %(message)s",
+        )
+        self.logger = logging.getLogger(self.NAME)
+
+    def info(self, message: str) -> None:
+        self.logger.info(message)
+
+    def debug(self, message: str) -> None:
+        self.logger.debug(message)
+
+    def warning(self, message: str) -> None:
+        self.logger.warning(message)
+
+    def error(self, message: str) -> None:
+        self.logger.error(message)
+
+    def critical(self, message: str) -> None:
+        self.logger.critical(message)
+
+
+LoggerType: TypeAlias = Union[ABCDLogger, bool]
+
+
+def construct_logger(logger: LoggerType) -> ABCDLogger:
+    if isinstance(logger, bool):
+        return StdOutLogger() if logger else NoOpLogger()
+
+    return logger
