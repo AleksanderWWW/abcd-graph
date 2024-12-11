74808c385c0587e397ad2650844efbfb8dd19285
AleksanderWWW <alwojnarowicz@gmail.com>
2024-12-11 15:51:28 +0100

    fix docker build and push

--- publish.sh
+++ publish.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 #  This script will build the package, and publish it to PyPI.
-#  It will also build and push docker images to GitHub Container Registry.
 
 set -euo pipefail
 
 if [ -z "$PYPI_TOKEN" ]
   then
     echo "PyPI token not provided. Exiting."
-    exit 1
-fi
-
-if [ -z "$GHCR_TOKEN" ]
-  then
-    echo "GHCR token not provided. Exiting."
     exit 1
 fi
 
@@ -27,20 +20,3 @@ poetry build
 poetry publish  --username __token__ --password "$PYPI_TOKEN"
 
 echo "Published $TAG to PyPI."
-
-echo "Running docker build-and-push"
-
-echo "Logging in to GitHub Container Registry"
-docker login --username AleksanderWWW --password "$GHCR_TOKEN" ghcr.io
-
-echo "Building basic docker image"
-docker build -t ghcr.io/aleksanderwww/abcd-graph:"$TAG" .
-
-echo "Pushing basic docker image"
-docker push ghcr.io/aleksanderwww/abcd-graph:"$TAG"
-
-echo "Building full docker image"
-docker build -t ghcr.io/aleksanderwww/abcd-graph-all:"$TAG" --build-arg INSTALL_TYPE=all .
-
-echo "Pushing full docker image"
-docker push ghcr.io/aleksanderwww/abcd-graph-all:"$TAG"
